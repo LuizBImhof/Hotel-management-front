ec273b1806796d5633891cdf39bc3efddf049db1
LuizBImhof <luiz.imhof@gmail.com>
2024-05-29 13:29:13 -0300

    feat: header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { GuestsListComponent } from './components/guests-list/guests-list.component';
 import { ReservationsListComponent } from './components/reservations-list/reservations-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatDivider } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     GuestsListComponent,
-    ReservationsListComponent
+    ReservationsListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatDivider
   ],
   providers: [
     provideAnimationsAsync()
